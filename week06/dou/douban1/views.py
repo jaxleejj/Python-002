fa62aacdfd4b943309e540e81458d7594e118a20
jaxleejj <451178735@qq.com>
2020-09-28 10:51:17 +0800

    20200928

--- week06/dou/douban1/views.py
+++ week06/dou/douban1/views.py
@@ -8,9 +8,7 @@ def index(request):
     return HttpResponse("Hello World ! this is test_page")
 
 def show_page(request):
-#麻烦老师请体谅，因工作繁忙，没按题目要求展示电影方面的资料
-#为了不耽误作业，大概按照作业的核心要点，快速学习整套django流程，
-#只好拿了老师视频中现成的 t1.sql 书评文件作为原始数据导入mysql，展示书评方面信息以完成作业
+
     to_show=T1.objects.all().filter(n_star__gte=3)
     return render(request, 'index.html', locals())
 
